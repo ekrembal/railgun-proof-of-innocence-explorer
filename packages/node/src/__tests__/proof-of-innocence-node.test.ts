d4ab10a513358bfad3bd6b3419891ec3374d86f0
jacobmakarsky <jacobmakarsky@gmail.com>
2023-11-27 16:00:59 +0000

    test coverage on proof-of-innocence-node

--- packages/node/src/__tests__/proof-of-innocence-node.test.ts
+++ packages/node/src/__tests__/proof-of-innocence-node.test.ts
@@ -6,21 +6,21 @@ import { TestMockListProviderExcludeSingleAddress } from '../tests/list-provider
 import { PollStatus } from '../models/general-types';
 import { poll } from '@railgun-community/shared-models';
 import { MOCK_LIST_KEYS } from '../tests/mocks.test';
+import sinon, { SinonStub } from 'sinon';
+import axios from 'axios';
 
 chai.use(chaiAsPromised);
-// const { expect } = chai;
-
-let nodeWithListProvider: ProofOfInnocenceNode;
-
-let nodeOnlyAggregator: ProofOfInnocenceNode;
 
 const PORT_1 = '3010';
 const PORT_2 = '3011';
 
 describe('proof-of-innocence-node', () => {
-  before(async function run() {
-    this.timeout(10000);
+  let nodeWithListProvider: ProofOfInnocenceNode;
 
+  let nodeOnlyAggregator: ProofOfInnocenceNode;
+
+  it('Should start up an aggregator node', async () => {
+    // Start the node
     nodeOnlyAggregator = new ProofOfInnocenceNode(
       '0.0.0.0',
       PORT_2,
@@ -28,24 +28,25 @@ describe('proof-of-innocence-node', () => {
       undefined,
     );
     await nodeOnlyAggregator.start();
+    await nodeOnlyAggregator.stop();
+  }).timeout(40000);
 
-    const testListProvider = new TestMockListProviderExcludeSingleAddress(
-      MOCK_LIST_KEYS[0],
+  it('Should start up a node with list provider', async () => {
+    // Start the aggregator
+    nodeOnlyAggregator = new ProofOfInnocenceNode(
+      '0.0.0.0',
+      PORT_2,
+      [], // No connected nodes
+      undefined,
     );
+    await nodeOnlyAggregator.start();
+    // Start the node
     nodeWithListProvider = new ProofOfInnocenceNode(
       '0.0.0.0',
       PORT_1,
       [{ name: 'test', nodeURL: `http://localhost:${PORT_2}` }],
-      testListProvider,
+      new TestMockListProviderExcludeSingleAddress(MOCK_LIST_KEYS[0]),
     );
-  });
-
-  after(async () => {
-    await nodeOnlyAggregator.stop();
-    await nodeWithListProvider.stop();
-  });
-
-  it('Should start up a node with list provider', async () => {
     await nodeWithListProvider.start();
 
     // Check that the node is an instance of ProofOfInnocenceNode
@@ -69,8 +70,53 @@ describe('proof-of-innocence-node', () => {
     await nodeWithListProvider.stop();
   }).timeout(30000);
 
-  it('Should start up a node with only aggregator', async () => {
-    await nodeOnlyAggregator.start();
-    await nodeOnlyAggregator.stop();
-  }).timeout(20000);
+  it('Should not start a list provider node if already running', async function () {
+    this.timeout(40000);
+
+    // Start the node
+    nodeWithListProvider = new ProofOfInnocenceNode(
+      '0.0.0.0',
+      PORT_1,
+      [{ name: 'test', nodeURL: `http://localhost:${PORT_2}` }],
+      new TestMockListProviderExcludeSingleAddress(MOCK_LIST_KEYS[0]),
+    );
+    await nodeWithListProvider.start();
+
+    // Wait for a short delay to ensure that the start method has finished
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Try to start the node for the second time
+    // This should return immediately without throwing any errors
+    await expect(nodeWithListProvider.start()).to.be.fulfilled;
+
+    // Stop the node
+    await nodeWithListProvider.stop();
+  });
+
+  it('Should not start a node if API fails to run', async function () {
+    this.timeout(40000);
+
+    // Start the node
+    nodeWithListProvider = new ProofOfInnocenceNode(
+      '0.0.0.0',
+      PORT_1,
+      [{ name: 'test', nodeURL: `http://localhost:${PORT_2}` }],
+      new TestMockListProviderExcludeSingleAddress(MOCK_LIST_KEYS[0]),
+    );
+
+    // Stub the axios.get method to throw an error
+    const stub: SinonStub = sinon.stub(axios, 'get');
+    stub.throws(new Error('Network Error'));
+
+    // This should throw an error because the API fails to run
+    await expect(nodeWithListProvider.start()).to.be.rejectedWith(
+      `Cannot start node: port ${PORT_1} is already in use`,
+    );
+
+    // Restore the axios.get method
+    stub.restore();
+
+    // Stop the node
+    await nodeWithListProvider.stop();
+  });
 });
